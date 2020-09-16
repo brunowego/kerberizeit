2bd2962f8c059361813eead8f74e40d14762356d
Bruno Wego <brunowego@gmail.com>
2020-09-16 17:56:21 -0300

    chore(shell): added install package functions

--- kerberizeit.sh
+++ kerberizeit.sh
@@ -34,6 +34,45 @@ fi
 echo "Platform: $PLATFORM"
 echo "Package manager: $PACKAGE_MANAGER"
 
-# 2. Install Dependencies based on OS release (/etc/os-release)
+# Install necessary packages
+function install_brew_packages() {
+    echo 'Installing brew packages...'
+
+    # Install dependencies
+    brew install krb5
+}
+
+function install_apt_packages() {
+    echo 'Installing apt packages...'
+
+    # Update
+    apt update
+
+    # Install dependencies
+    DEBIAN_FRONTEND='noninteractive' apt -y install krb5-user libkrb5-dev
+}
+
+function install_yum_packages() {
+    echo 'Installing yum packages...'
+
+    # Update
+    yum check-update
+
+    # Install dependencies
+    yum -y install krb5-workstation krb5-devel
+}
+
+function install_apk_packages() {
+    echo 'Installing apk packages...'
+
+    # Update
+    apk update
+
+    # Install dependencies
+    apk add krb5 krb5-dev
+}
+
+eval "type install_${PACKAGE_MANAGER}_packages &>/dev/null && install_${PACKAGE_MANAGER}_packages"
+
 # 3. Create krb5 conf based on environment variables
 # 4. Init Kerberos based on selected auth method (Password/PAM/Keytab)
